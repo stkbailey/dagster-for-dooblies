d4ede91fcda7bcaca5b34ab7a2187c7be6ac4473
Stephen Bailey <7850417+stkbailey@users.noreply.github.com>
2022-05-30 14:37:17 -0400

    Add terraform (#1)

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,6 +5,13 @@ terraform {
       version = ">= 4.0.0"
     }
   }
+  cloud {
+    organization = "stkbailey"
+    workspaces {
+      name = "dagster-for-dooblies"
+
+    }
+  }
 }
 
 provider "aws" {
@@ -14,99 +21,22 @@ provider "aws" {
 }
 
 locals {
-  name   = "metrics"
+  cluster_name   = "dagster"
 }
 
+module "ecs" {
+  source = "terraform-aws-modules/ecs/aws"
+  name = local.cluster_name
+  container_insights = true
+  capacity_providers = ["FARGATE", "FARGATE_SPOT"]
 
-module "vpc" {
-  source  = "terraform-aws-modules/vpc/aws"
-  version = "~> 3"
-
-  name = local.name
-  cidr = "10.99.0.0/18"
-
-  azs              = ["${var.region}a", "${var.region}b", "${var.region}c"]
-  public_subnets   = ["10.99.0.0/24", "10.99.1.0/24", "10.99.2.0/24"]
-  private_subnets  = ["10.99.3.0/24", "10.99.4.0/24", "10.99.5.0/24"]
-  database_subnets = ["10.99.7.0/24", "10.99.8.0/24", "10.99.9.0/24"]
-
-  # make public access
-  # https://registry.terraform.io/modules/terraform-aws-modules/vpc/aws/latest#public-access-to-rds-instances
-  create_database_subnet_group = true
-  create_database_subnet_route_table     = true
-  create_database_internet_gateway_route = true
-  enable_dns_hostnames = true
-  enable_dns_support   = true
-  map_public_ip_on_launch = true
-
-  public_subnet_tags = {
-    Name = "${local.name}-public"
-  }
-
-  vpc_tags = {
-    Name = "vpc-${local.name}"
-  }
-}
-
-module "security_group" {
-  source  = "terraform-aws-modules/security-group/aws"
-  version = "~> 4"
-
-  name        = local.name
-  description = "Complete PostgreSQL example security group"
-  vpc_id      = module.vpc.vpc_id
-
-  # ingress
-  ingress_with_cidr_blocks = [
+  default_capacity_provider_strategy = [
     {
-      from_port   = 5432
-      to_port     = 5432
-      protocol    = "tcp"
-      description = "PostgreSQL access from within VPC"
-      cidr_blocks = module.vpc.vpc_cidr_block
-    },
-    {
-      from_port   = 5432
-      to_port     = 5432
-      protocol    = "tcp"
-      description = "PostgreSQL access from all addresses"
-      cidr_blocks = "0.0.0.0/0"
+      capacity_provider = "FARGATE_SPOT"
     }
   ]
 
-}
-
-module "db" {
-  source  = "terraform-aws-modules/rds/aws"
-  version = "~> 3.5"
-
-  identifier = "${local.name}-default"
-
-  create_db_option_group    = false
-  create_db_parameter_group = false
-
-  # All available versions: https://docs.aws.amazon.com/AmazonRDS/latest/UserGuide/CHAP_PostgreSQL.html#PostgreSQL.Concepts
-  engine               = "postgres"
-  engine_version       = "13.1"
-  family               = "postgres13" # DB parameter group
-  major_engine_version = "13"         # DB option group
-  instance_class       = "db.t3.micro"
-
-  allocated_storage = 20
-
-  # NOTE: Do NOT use 'user' as the value for 'username' as it throws:
-  # "Error creating DB Instance: InvalidParameterValue: MasterUsername≠
-  # user cannot be used as it is a reserved word used by the engine"
-  name                   = "admin_user"
-  username               = "admin_user"
-  create_random_password = true
-  random_password_length = 24
-  port                   = 5432
-
-  create_db_subnet_group = false
-  db_subnet_group_name   = module.vpc.database_subnet_group
-  vpc_security_group_ids = [module.security_group.security_group_id]
-  publicly_accessible = true
-
-  apply_immediately = true
+  tags = {
+    Environment = "Development"
+  }
 }
